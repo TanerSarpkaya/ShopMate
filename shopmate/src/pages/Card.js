e0bf5cb956752af1d516d2480344685a0318b355
TanerSarpkaya <sarpkayataner@gmail.com>
2023-08-29 17:39:23 +0300

    I tried commit

--- shopmate/src/pages/Card.js
+++ shopmate/src/pages/Card.js
@@ -8,7 +8,8 @@ export const Card = () =>{
     useTitle("Cart");
     const products = [
         {"id": 1, "name": "Sony Wh-Ch510 Bluetooth Wireless", "price": 149, "image": "/assets/images/1001.png"},
-        {"id": 2, "name": "boAt Rockerz 450", "price": 49, "image": "/assets/images/1002.png"}
+        {"id": 2, "name": "boAt Rockerz 450", "price": 49, "image": "/assets/images/1002.png"},
+        {"id": 3, "name": "JBL Tune 760NC", "price": 179, "image": "/assets/images/1003.png"}
     ]
     return(
         <main>
